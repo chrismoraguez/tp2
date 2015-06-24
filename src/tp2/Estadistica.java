6f5aa0da1bc276772f8b333ad999ce43ab8b285e
Chris <moraguez.christian@maimonides.edu>
2015-06-24 02:12:04 -0300

    Change/s: Creación de repositorio y agregado de clases.

---
+++ src/tp2/Estadistica.java
@@ -0,0 +1,28 @@
+package tp2;
+
+public class Estadistica {
+	
+	String uuid;
+	int debitos;
+	int creditos;
+	
+	public int getDebitos() {
+		return debitos;
+	}
+	public void setDebitos(int debitos) {
+		this.debitos = debitos;
+	}
+	public int getCreditos() {
+		return creditos;
+	}
+	public void setCreditos(int creditos) {
+		this.creditos = creditos;
+	}
+	
+	public String getUuid() {
+		return uuid;
+	}
+	public void setUuid(String uuid) {
+		this.uuid = uuid;
+	}
+}
